70895bd520f4b10811bbc5f08146bb93eae1fc00
nadavdor15 <46018347+nadavdor15@users.noreply.github.com>
2019-03-11 23:55:36 +0200

    Check

---
+++ AP2_Ex1/AP2_Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP2_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Console.WriteLine(a);
+        }
+    }
+}
